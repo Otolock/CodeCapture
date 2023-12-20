c762398bd3a8ea84d51059234ee4cd36fab378a1
Antonio Santos <aj.santos128@gmail.com>
2023-12-19 23:37:59 -0400

    Implement CodeEditor and Live Editing

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,67 @@
-import Container from '@/components/Container'
-import Header from '@/components/Header'
+'use client'
+
+import React, { useRef, useState, useCallback } from 'react';
+import * as htmlToImage from 'html-to-image';
+import CodeEditor from '@/components/CodeEditor';
+import CodeBlock from '@/components/CodeBlock';
+import Container from '@/components/Container';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export default function Home() {
+  const [language, setLanguage] = useState('javascript');
+  const [code, setCode] = useState(`
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <h1>Hello, world!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+  `.trim()); // Initialize code state with sample code
+
+  const codeBlockRef = useRef();
+
+  const convertToImage = useCallback(() => {
+    if (codeBlockRef.current) {
+      htmlToImage.toBlob(codeBlockRef.current, { cacheBust: true })
+        .then((blob) => {
+          const url = URL.createObjectURL(blob);
+          const link = document.createElement('a');
+          link.download = 'code-snippet.png';
+          link.href = url;
+          link.click();
+          URL.revokeObjectURL(url); // Clean up the URL object
+        })
+        .catch((error) => {
+          console.error('oops, something went wrong!', error);
+        });
+    }
+  }, [codeBlockRef]);
+
   return (
     <>
       <Header />
       <Container>
-
+        <CodeEditor
+          language={language}
+          setLanguage={setLanguage}
+          code={code}
+          onChange={setCode}
+        />
+        <div ref={codeBlockRef} className="mb-4"> {/* Added margin-bottom here */}
+          <CodeBlock key={code} code={code} language={language} />
+        </div>
+        <button
+          onClick={convertToImage}
+          className='rounded-md bg-emerald-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600'
+        >
+          Download Image
+        </button>
       </Container>
+      <Footer version={0.1}/>
     </>
-  )
+
+  );
 }
---
+++ src/components/CodeEditor.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Editor from '@monaco-editor/react';
+
+const CodeEditor = ({ language, setLanguage, code, onChange }) => {
+    const handleEditorChange = (value, event) => {
+        if (onChange) {
+            onChange(value);
+        }
+    };
+
+    const languages = [
+        { label: 'JavaScript', value: 'javascript' },
+        { label: 'HTML', value: 'html' },
+        { label: 'CSS', value: 'css' },
+        // Add more languages as needed
+    ];
+
+    return (
+        <div>
+            <div>
+                <label htmlFor="language" className="block text-sm font-medium leading-6 text-gray-900">
+                    Language
+                </label>
+                <select
+                    id="language"
+                    name="language"
+                    value={language}
+                    onChange={(e) => setLanguage(e.target.value)}
+                    className="my-2 block rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-emerald-600 sm:text-sm sm:leading-6"
+                    defaultValue="Javascript"
+                >
+                    {languages.map(lang => (
+                        <option key={lang.value} value={lang.value}>
+                            {lang.label}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <Editor
+                height="500px"
+                language={language}
+                value={code}
+                onChange={handleEditorChange}
+                theme="vs-dark"
+                options={{
+                    minimap: { enabled: false }
+                }}
+            />
+        </div>
+    );
+};
+
+export default CodeEditor;
