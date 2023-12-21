40fc554f633c7013ed492cfbd99e3907ff4b068b
Antonio Santos <aj.santos128@gmail.com>
2023-12-20 21:11:49 -0400

    Update CodeEditor component to use CodeMirror

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,19 @@
 'use client'
 
-import React, { useRef, useState, useCallback } from 'react';
+import React, { useRef, useCallback } from 'react';
 import { toPng } from 'html-to-image';
 import CodeEditor from '@/components/CodeEditor';
-import CodeBlock from '@/components/CodeBlock';
 import Container from '@/components/Container';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
 export default function Home() {
-  const [language, setLanguage] = useState('javascript');
-  const [code, setCode] = useState(`
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-function App() {
-  return <h1>Hello, world!</h1>;
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
-  `.trim()); // Initialize code state with sample code
-
-  const codeBlockRef = useRef();
+  const codeEditorRef = useRef();
 
   const convertToImage = useCallback(() => {
-    if (codeBlockRef.current) {
-      const width = codeBlockRef.current.scrollWidth; // Get the full width of the element
-      toPng(codeBlockRef.current, { cacheBust: true, width })
+    if (codeEditorRef.current) {
+      const width = codeEditorRef.current.scrollWidth; // Get the full width of the element
+      toPng(codeEditorRef.current, { cacheBust: true, width })
         .then((dataUrl) => {
           const link = document.createElement('a');
           link.download = 'code-snippet.png';
@@ -37,30 +24,21 @@ ReactDOM.render(<App />, document.getElementById('root'));
           console.error('oops, something went wrong!', error);
         });
     }
-  }, [codeBlockRef]);
+  }, [codeEditorRef]);
   
-
   return (
     <>
       <Header />
       <Container>
-        <CodeEditor
-          language={language}
-          setLanguage={setLanguage}
-          code={code}
-          onChange={setCode}
-        />
-        <div ref={codeBlockRef} className="mb-4">
-          <CodeBlock key={code} code={code} language={language} />
-        </div>
+        <CodeEditor ref={codeEditorRef} />
         <button
           onClick={convertToImage}
-          className='rounded-md bg-emerald-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600'
+          className='rounded-md bg-emerald-600 mt-4 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600'
         >
           Download Image
         </button>
       </Container>
-      <Footer version={0.2} />
+      <Footer version={0.3} />
     </>
   );
 }
