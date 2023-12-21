c3f20a0749c6d6b5af2c676b3ef81197a60ff0ce
Antonio Santos <aj.santos128@gmail.com>
2023-12-20 22:09:38 -0400

    Fix container resizing for mobile

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,7 @@
 export default function Container({ children }) {
   return (
     <main>
-      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">{children}</div>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">{children}</div>
     </main>
   );
 }
